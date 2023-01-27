e55f5460d8951e2a8c845e84168d9bd5aa2ac754
Vladimir <vova2066@yandex.ru>
2023-01-27 22:30:20 +0300

    add app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+
 function App() {
+
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if(todo !== '') {
+      setTodos([...todos, todo]);
+      setTodo('');
+    }
+  }
+
+  const deleteTodo = (todo) => {
+    const newTodos = todos.filter(item => item !== todo);
+    setTodos(newTodos)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>React Todo App</h1>
+      <div className="input-wrapper">
+        <input 
+          placeholder="enter todo"
+          value={todo}
+          onChange={(evt) => setTodo(evt.target.value)}
+        />
+        <button 
+        className="add-button"
+        onClick={addTodo}
+        >Add todo</button>
+      </div>
+
+      {
+        todos?.length > 0 ? (
+          <ul className="todo-list">
+
+      {todos.map(todo => (
+          <div className="todo">
+            <li>{todo}</li>
+            <button 
+            className="delete-button"
+            onClick={() => deleteTodo(todo)}
+            >Delete</button>
+          </div>
+        ))}
+
+      </ul>
+        ) : (
+          <div className="empty">Список дел пуст</div>
+        ) 
+      }
+
+
     </div>
   );
 }
